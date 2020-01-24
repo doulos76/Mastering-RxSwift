4d17d9594d620d7896ba6fa1c416365e7006d537
구민회 <inojyes@gmail.com>
2020-01-24 16:19:52 +0900

    add chapter6: transforming operator

--- main/Operators/Transforming Operators/flatMapFirst.playground/Contents.swift
+++ main/Operators/Transforming Operators/flatMapFirst.playground/Contents.swift
@@ -35,9 +35,9 @@ let b = BehaviorSubject(value: 2)
 let subject = PublishSubject<BehaviorSubject<Int>>()
 
 subject
-   .flatMap { $0.asObservable() }
-   .subscribe { print($0) }
-   .disposed(by: disposeBag)
+  .flatMapFirst { $0.asObservable() }
+  .subscribe { print($0) }
+  .disposed(by: disposeBag)
 
 subject.onNext(a)
 subject.onNext(b)
--- main/Operators/Transforming Operators/flatMapLatest.playground/Contents.swift
+++ main/Operators/Transforming Operators/flatMapLatest.playground/Contents.swift
@@ -35,7 +35,13 @@ let b = BehaviorSubject(value: 2)
 let subject = PublishSubject<BehaviorSubject<Int>>()
 
 subject
-   .flatMap { $0.asObservable() }
+   .flatMapLatest { $0.asObservable() }
    .subscribe { print($0) }
    .disposed(by: disposeBag)
 
+subject.onNext(a)
+a.onNext(11)
+subject.onNext(b)
+b.onNext(22)
+a.onNext(111)
+subject.onNext(a)
