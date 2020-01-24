d4e12440099a27934801f8727ba4a22b0a386e82
구민회 <inojyes@gmail.com>
2020-01-24 11:56:55 +0900

    add chatper1 - hello,RxSwift, Reactive Programming

--- main/Hello/Hello.playground/Contents.swift
+++ main/Hello/Hello.playground/Contents.swift
@@ -23,26 +23,22 @@
 import UIKit
 import RxSwift
 
+let disposeBag = DisposeBag()
 
+//Observable.just("Hello, RxSwift")
+//  .subscribe { print($0) }
+//  .disposed(by: disposeBag)
 
+//var a = 1
+//var b = 2
+//a + b
+//a = 12
 
+let a = BehaviorSubject(value: 1)
+let b = BehaviorSubject(value: 2)
 
+Observable.combineLatest(a, b) { $0 + $1 }
+  .subscribe(onNext: { print($0) })
+  .disposed(by: disposeBag)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+a.onNext(12)
--- main/KeyConcepts/Observables.playground/Contents.swift
+++ main/KeyConcepts/Observables.playground/Contents.swift
@@ -28,15 +28,3 @@ import RxSwift
  */
 
 
-
-
-
-
-
-
-
-
-
-
-
-
