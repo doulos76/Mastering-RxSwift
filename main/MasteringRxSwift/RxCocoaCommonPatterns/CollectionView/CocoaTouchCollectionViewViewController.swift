0749aa4a17f10d8f626908af7d37f4751a917691
구민회 <inojyes@gmail.com>
2020-01-25 00:06:41 +0900

    add chapter rx cocoa common patterns - tableview, collection view

--- main/MasteringRxSwift/RxCocoaCommonPatterns/CollectionView/CocoaTouchCollectionViewViewController.swift
+++ main/MasteringRxSwift/RxCocoaCommonPatterns/CollectionView/CocoaTouchCollectionViewViewController.swift
@@ -23,47 +23,47 @@
 import UIKit
 
 class CocoaTouchCollectionViewViewController: UIViewController {
-   
-   let list = MaterialBlue.allColors
-   
-   override func viewDidLoad() {
-      super.viewDidLoad()
-      
-      
-   }
+  
+  let list = MaterialBlue.allColors
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
 }
 
 extension CocoaTouchCollectionViewViewController: UICollectionViewDataSource {
-   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-      return list.count
-   }
-   
-   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-      let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "colorCell", for: indexPath) as! ColorCollectionViewCell
-      
-      cell.backgroundColor = list[indexPath.item]
-      cell.hexLabel.text = list[indexPath.item].rgbHexString
-      
-      return cell
-   }
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return list.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "colorCell", for: indexPath) as! ColorCollectionViewCell
+    
+    cell.backgroundColor = list[indexPath.item]
+    cell.hexLabel.text = list[indexPath.item].rgbHexString
+    
+    return cell
+  }
 }
 
 extension CocoaTouchCollectionViewViewController: UICollectionViewDelegate {
-   func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-      collectionView.deselectItem(at: indexPath, animated: true)
-      
-      print(list[indexPath.item].rgbHexString)
-   }
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    collectionView.deselectItem(at: indexPath, animated: true)
+    
+    print(list[indexPath.item].rgbHexString)
+  }
 }
 
 
 extension CocoaTouchCollectionViewViewController: UICollectionViewDelegateFlowLayout {
-   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-      guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else {
-         return CGSize.zero
-      }
-      
-      let value = (collectionView.frame.width - (flowLayout.sectionInset.left + flowLayout.sectionInset.right + flowLayout.minimumInteritemSpacing)) / 2
-      return CGSize(width: value, height: value)
-   }
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else {
+      return CGSize.zero
+    }
+    
+    let value = (collectionView.frame.width - (flowLayout.sectionInset.left + flowLayout.sectionInset.right + flowLayout.minimumInteritemSpacing)) / 2
+    return CGSize(width: value, height: value)
+  }
 }
